90d660f631122e30168ef6a046fc3a64f0b68347
neodain-net <neodain.net@gmail.com>
2025-05-16 18:31:18 +0900

    github action test

--- user_rest/src/main/java/com/neodain/user_rest/UserRestApplication.java
+++ user_rest/src/main/java/com/neodain/user_rest/UserRestApplication.java
@@ -2,12 +2,20 @@ package com.neodain.user_rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class UserRestApplication {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello, World!";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(UserRestApplication.class, args);
     }
-
 }
