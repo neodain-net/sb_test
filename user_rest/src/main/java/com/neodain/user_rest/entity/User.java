a8bb0c66a15eda167476129134fcf11d91b81384
neodain-net <neodain.net@gmail.com>
2025-05-16 21:42:30 +0900

    git hub workflow test

--- user_rest/src/main/java/com/neodain/user_rest/entity/User.java
+++ user_rest/src/main/java/com/neodain/user_rest/entity/User.java
@@ -11,7 +11,6 @@ import java.util.UUID;
 @Setter
 @EntityScan
 @AllArgsConstructor  // 모든 Arg 가 있는 생성자 자동 생성
-//@NoArgsConstructor   // Arg가 없는 생성자 자동 생성
 public class User {
     private String id;
     private String name;
@@ -27,25 +26,7 @@ public class User {
         this.email = email;
     }
 
-//    public User(String id, String name, String email) {
-//        this.id = id;
-//        this.name = name;
-//        this.email = email;
-//    }
-
-    public static User sampleUser() {
-        return new User("Kim", "kim@neodain.net");
-    }
-
-    public static User sampleUser(String name, String email) {
-        return new User(name, email);
-    }
-
     public static void main(String[] args) {
         User user = new User("Park", "park@neodain.net");
-//        System.out.println(new Gson().toJson(user));
-//        System.out.println(new Gson().toJson(sampleUser()));
-//        System.out.println(new Gson().toJson(sampleUser("Kim", "kim@neodain.net")));
-//        System.out.println(new Gson().toJson(sampleUser("Lee", "lee@google.com")));
     }
 }
